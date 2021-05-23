23819fb89b75285b33adf028de70f0a562cbac70
Nightmare-MY <906262255@qq.com>
2021-05-24 01:31:07 +0800

    Create app_routes.dart

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,8 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  static const prefix = '/speedshare';
+  static const HOME = '$prefix/home';
+  static const chat = '$prefix/chat';
+}
