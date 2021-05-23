ec064b8a3dcb5c2182a277cca49072c2b78f72ce
Nightmare-MY <906262255@qq.com>
2021-05-24 01:30:43 +0800

    add navigator page

---
+++ lib/pages/navigator_page.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:global_repository/global_repository.dart';
+import 'package:speed_share/themes/default_theme_data.dart';
+
+import 'home_page.dart';
+import 'share_chat.dart';
+
+class NavigatorPage extends StatefulWidget {
+  @override
+  _NavigatorPageState createState() => _NavigatorPageState();
+}
+
+class _NavigatorPageState extends State<NavigatorPage> {
+  int pageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color(0xffede8f8),
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: const TextStyle(
+          // fontWeight: FontWeight.bold,
+          color: fontColor,
+          fontSize: 12,
+        ),
+        unselectedLabelStyle: const TextStyle(
+          // fontWeight: FontWeight.bold,
+          color: fontColor,
+          fontSize: 12,
+        ),
+        unselectedIconTheme: IconThemeData(
+          color: fontColor,
+        ),
+        enableFeedback: false,
+        unselectedItemColor: fontColor,
+        selectedItemColor: fontColor,
+        elevation: 0.0,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Container(
+              width: 64,
+              height: 32,
+              child: Icon(
+                Icons.open_in_browser,
+              ),
+            ),
+            activeIcon: Container(
+              width: 64,
+              height: 32,
+              decoration: BoxDecoration(
+                color: Color(0xffcfbff7),
+                borderRadius: BorderRadius.circular(14),
+              ),
+              child: Icon(
+                Icons.open_in_browser,
+              ),
+            ),
+            label: '文件部署',
+          ),
+          BottomNavigationBarItem(
+            activeIcon: Container(
+              width: 64,
+              height: 32,
+              decoration: BoxDecoration(
+                color: Color(0xffcfbff7),
+                borderRadius: BorderRadius.circular(14),
+              ),
+              child: Icon(Icons.share),
+            ),
+            icon: Container(
+              width: 64,
+              height: 32,
+              child: Icon(Icons.share),
+            ),
+            label: '共享窗',
+          ),
+        ],
+        currentIndex: pageIndex,
+        onTap: (int index) {
+          if (index == 1) {
+            Get.to(ShareChat());
+            return;
+          }
+          pageIndex = index;
+          setState(() {});
+        },
+      ),
+      body: [
+        HomePage(),
+        ShareChat(),
+      ][pageIndex],
+    );
+  }
+}
