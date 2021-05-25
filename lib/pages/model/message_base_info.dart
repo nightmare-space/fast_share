dfb81579b7a8fdc08fb1bf4f6943042f8ef8a814
Nightmare-MY <906262255@qq.com>
2021-05-26 00:02:07 +0800

    add field

--- lib/pages/model/message_base_info.dart
+++ lib/pages/model/message_base_info.dart
@@ -4,13 +4,15 @@ class MessageBaseInfo {
   String type;
   String data;
   String msgType;
+  List<dynamic> address;
 
-  MessageBaseInfo({this.type, this.data, this.msgType});
+  MessageBaseInfo({this.type, this.data, this.msgType, this.address});
 
   MessageBaseInfo.fromJson(Map<String, dynamic> json) {
     type = json['type'];
     data = json['data'];
     msgType = json['msgType'];
+    address = json['address'];
   }
 
   Map<String, dynamic> toJson() {
@@ -18,6 +20,7 @@ class MessageBaseInfo {
     map['type'] = type;
     map['data'] = data;
     map['msgType'] = msgType;
+    map['address'] = address;
     return map;
   }
 
