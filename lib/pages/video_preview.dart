68b248a5f93ce2c73691bd2215bfba4880ccbd83
Nightmare-MY <mengyanshou@gmail.com>
2021-05-25 18:48:02 +0800

    add video preview page

---
+++ lib/pages/video_preview.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flick_video_player/flick_video_player.dart';
+import 'package:video_player/video_player.dart';
+
+class SamplePlayer extends StatefulWidget {
+  final String url;
+  SamplePlayer({Key key, this.url}) : super(key: key);
+
+  @override
+  _SamplePlayerState createState() => _SamplePlayerState();
+}
+
+class _SamplePlayerState extends State<SamplePlayer> {
+  FlickManager flickManager;
+  @override
+  void initState() {
+    super.initState();
+    flickManager = FlickManager(
+      videoPlayerController: VideoPlayerController.network(widget.url),
+    );
+  }
+
+  @override
+  void dispose() {
+    flickManager.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FlickVideoPlayer(flickManager: flickManager),
+    );
+  }
+}
