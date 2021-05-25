30bb6a430ba147ce54faf56e9f00701f5c255815
Nightmare-MY <906262255@qq.com>
2021-05-26 00:02:44 +0800

    support get history message

--- lib/utils/chat_server.dart
+++ lib/utils/chat_server.dart
@@ -1,12 +1,21 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:get_server/get_server.dart';
 import 'package:global_repository/global_repository.dart';
 
 void createChatServer() {
   Log.d('chat server starting...');
+  String home = '';
+  if (GetPlatform.isDesktop) {
+    home = '/Users/' + Platform.environment['USER'];
+  } else {
+    home = '/sdcard/';
+  }
   runApp(
     GetServerApp(
       port: 7000,
-      // home: FolderWidget('/sdcard/'),
+      home: FolderWidget(home),
       getPages: [
         GetPage(name: '/chat', page: () => SocketPage()),
       ],
@@ -16,12 +25,14 @@ void createChatServer() {
 }
 
 class SocketPage extends GetView {
+  List<String> msgs = [];
   @override
   Widget build(BuildContext context) {
     return Socket(
       builder: (socket) {
+        socket.rawSocket;
         socket.onOpen((ws) {
-          Log.d('${ws.id}已连接');
+          Log.d('${ws.id} 已连接');
           // ws.send('socket ${ws.id} connected');
         });
         socket.on('join', (val) {
@@ -32,8 +43,16 @@ class SocketPage extends GetView {
           }
         });
         socket.onMessage((data) {
-          // 分msgtype，img，tip，text
+          Log.e(data);
+          if (json.decode(data)['type'] == 'getHistory') {
+            Log.e('获取历史消息');
+            msgs.forEach((element) {
+              socket.send(element);
+            });
+            return;
+          }
           print('data: $data');
+          msgs.add(data);
           socket.broadcast(data);
         });
         socket.onClose((close) {
