baae1a069a83c0e6abab312cfdf1b4b53fd1b6b0
Nightmare-MY <906262255@qq.com>
2021-05-26 00:03:04 +0800

    support get path on macOS

--- lib/utils/shelf_static.dart
+++ lib/utils/shelf_static.dart
@@ -1,19 +1,32 @@
+import 'dart:io';
+
+import 'package:get/get.dart';
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_static/shelf_static.dart';
 import 'package:speed_share/config/config.dart';
 
+bool _started = false;
+
 class ShelfStatic {
   static Future<void> start() async {
-    var handler = createStaticHandler('/sdcard', listDirectories: true);
-
+    if (_started) {
+      return;
+    }
+    String home = '';
+    if (GetPlatform.isDesktop) {
+      home = '/Users/' + Platform.environment['USER'];
+    } else {
+      home = '/sdcard/';
+    }
+    var handler = createStaticHandler(home, listDirectories: true);
     io.serve(
       handler,
       '0.0.0.0',
       Config.shelfPort,
+      shared: true,
     );
+    _started = true;
   }
 
-  static void close() {
-    // requestServer.close();
-  }
+  static void close() {}
 }
