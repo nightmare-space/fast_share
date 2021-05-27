9010b4318cf3064391bea8cbd2aabd3a54ece633
Nightmare-MY <mengyanshou@gmail.com>
2021-05-27 15:53:54 +0800

    Update model.dart

--- lib/pages/model/model.dart
+++ lib/pages/model/model.dart
@@ -1,6 +1,4 @@
 export 'message_base_info.dart';
-export 'message_img_info.dart';
-export 'message_video_info.dart';
 export 'message_text_info.dart';
-export 'message_other_info.dart';
+export 'message_file_info.dart';
 export 'message_tip_info.dart';
