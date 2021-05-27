9e703993e4a54ba26a320d5840d30b5cb8979d24
Nightmare-MY <mengyanshou@gmail.com>
2021-05-27 15:53:51 +0800

    model refactoring

---
+++ lib/pages/model/message_file_info.dart
@@ -0,0 +1,66 @@
+import 'message_base_info.dart';
+
+class MessageFileInfo extends MessageBaseInfo {
+  String fileName;
+  String filePath;
+  String fileSize;
+  MessageFileInfo({
+    this.fileName,
+    this.fileSize,
+    String msgType,
+    String type,
+    String data,
+  }) : super(
+          data: data,
+          type: type,
+          msgType: msgType,
+        );
+
+  MessageFileInfo.fromJson(Map<String, dynamic> json) : super.fromJson(json) {
+    fileName = json['fileName'];
+    filePath = json['filePath'];
+    fileSize = json['fileSize'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = super.toJson();
+    data['fileName'] = fileName;
+    data['filePath'] = filePath;
+    data['fileSize'] = fileSize;
+    return data;
+  }
+}
+
+class MessageImgInfo extends MessageFileInfo {
+  MessageImgInfo({
+    String msgType,
+    String type,
+    String data,
+  }) : super(
+          data: data,
+          type: type,
+          msgType: msgType,
+        );
+
+  MessageImgInfo.fromJson(Map<String, dynamic> json) : super.fromJson(json) {}
+
+  Map<String, dynamic> toJson() {
+    final data = super.toJson();
+    return data;
+  }
+}
+
+class MessageVideoInfo extends MessageFileInfo {
+  String thumbnailPath;
+  MessageVideoInfo({this.thumbnailPath});
+
+  MessageVideoInfo.fromJson(Map<String, dynamic> json) : super.fromJson(json) {
+    thumbnailPath = json['thumbnailPath'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = super.toJson();
+    data['thumbnailPath'] = thumbnailPath;
+    return data;
+  }
+}
