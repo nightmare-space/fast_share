907a1223c930d2665fe5990cddb63b560b4f1512
Nightmare-MY <mengyanshou@gmail.com>
2021-05-27 15:53:36 +0800

    remove field address

--- lib/pages/model/message_base_info.dart
+++ lib/pages/model/message_base_info.dart
@@ -4,15 +4,17 @@ class MessageBaseInfo {
   String type;
   String data;
   String msgType;
-  List<dynamic> address;
 
-  MessageBaseInfo({this.type, this.data, this.msgType, this.address});
+  MessageBaseInfo({
+    this.type,
+    this.data,
+    this.msgType,
+  });
 
   MessageBaseInfo.fromJson(Map<String, dynamic> json) {
     type = json['type'];
     data = json['data'];
     msgType = json['msgType'];
-    address = json['address'];
   }
 
   Map<String, dynamic> toJson() {
@@ -20,7 +22,6 @@ class MessageBaseInfo {
     map['type'] = type;
     map['data'] = data;
     map['msgType'] = msgType;
-    map['address'] = address;
     return map;
   }
 
