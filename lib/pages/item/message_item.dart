cf44a59c46f06017bb58a798182f7163a0241f56
Nightmare-MY <mengyanshou@gmail.com>
2021-05-30 14:39:01 +0800

    Update message_item.dart

--- lib/pages/item/message_item.dart
+++ lib/pages/item/message_item.dart
@@ -1,26 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:global_repository/global_repository.dart';
-import 'package:speed_share/pages/item/image_item.dart';
 import 'package:speed_share/pages/item/text_item.dart';
 import 'package:speed_share/pages/model/model.dart';
-import '../video_preview.dart';
-import 'other_item.dart';
-import 'video_item.dart';
+import 'file_item.dart';
 
 Widget messageItem(MessageBaseInfo info, bool sendByUser) {
   Widget child;
-  if (info is MessageImgInfo) {
-    child = ImageItem(
-      info: info,
-      sendByUser: sendByUser,
-    );
-  } else if (info is MessageVideoInfo) {
-    child = VideoItem(
-      info: info,
-      sendByUser: sendByUser,
-    );
-  } else if (info is MessageTextInfo) {
+  if (info is MessageTextInfo) {
     child = TextMessageItem(
       data: info.content,
       sendByUser: sendByUser,
@@ -36,7 +21,7 @@ Widget messageItem(MessageBaseInfo info, bool sendByUser) {
       ),
     );
   } else if (info is MessageFileInfo) {
-    child = OtherItem(
+    child = FileItem(
       info: info,
       sendByUser: sendByUser,
     );
