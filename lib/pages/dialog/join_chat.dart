3c4f8e9afdb50a9aeced3eafe16e3efc0c726705
Nightmare-MY <mengyanshou@gmail.com>
2021-05-30 14:29:30 +0800

    let init text empty

--- lib/pages/dialog/join_chat.dart
+++ lib/pages/dialog/join_chat.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:global_repository/global_repository.dart';
 import 'package:speed_share/app/routes/app_pages.dart';
-import 'package:speed_share/pages/share_chat.dart';
-import 'package:speed_share/themes/default_theme_data.dart';
 
 class JoinChat extends StatefulWidget {
   @override
@@ -11,7 +10,7 @@ class JoinChat extends StatefulWidget {
 
 class _JoinChatState extends State<JoinChat> {
   TextEditingController controller = TextEditingController(
-    text: 'http://',
+    text: '',
   );
   @override
   void dispose() {
@@ -23,6 +22,8 @@ class _JoinChatState extends State<JoinChat> {
   Widget build(BuildContext context) {
     return Center(
       child: Material(
+        clipBehavior: Clip.antiAlias,
+        borderRadius: BorderRadius.circular(12),
         child: SizedBox(
           height: 180,
           width: 260,
@@ -30,11 +31,12 @@ class _JoinChatState extends State<JoinChat> {
             padding: const EdgeInsets.all(8.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
                   '请输入文件共享窗口地址',
                   style: TextStyle(
-                    color: accentColor,
+                    color: Colors.black,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
@@ -46,6 +48,10 @@ class _JoinChatState extends State<JoinChat> {
                   decoration: InputDecoration(
                     fillColor: Color(0xfff0f0f0),
                     helperText: '这个地址在创建窗口的时候会提示',
+                    hintText: '请输入共享窗口的URL',
+                    hintStyle: TextStyle(
+                      fontSize: 12,
+                    ),
                   ),
                 ),
                 SizedBox(
@@ -55,10 +61,13 @@ class _JoinChatState extends State<JoinChat> {
                   alignment: Alignment.centerRight,
                   child: TextButton(
                     onPressed: () {
+                      if (controller.text.isEmpty) {
+                        showToast('URL不能为空');
+                        return;
+                      }
                       Get.back();
                       Get.toNamed(
-                        Routes.chat +
-                            '?needCreateChatServer=false&chatServerAddress=${controller.text}',
+                        '${Routes.chat}?needCreateChatServer=false&chatServerAddress=${controller.text}',
                       );
                       // Get.to(ShareChat(
                       //   needCreateChatServer: false,
