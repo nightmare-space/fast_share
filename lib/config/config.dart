e88309779431cd0e6407b01977cfbb285ccf64ea
Nightmare-MY <mengyanshou@gmail.com>
2021-06-03 21:24:15 +0800

    Update config.dart

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -2,4 +2,8 @@ class Config {
   Config._();
   static int httpServerPort = 8001;
   static int shelfPort = 8002;
+  static int chatPort = 7000;
+  // static InternetAddress multicastAddress = InternetAddress('224.0.0.1');
+  // flutter package名，因为这个会影响assets的路径
+  static String flutterPackage = '';
 }
