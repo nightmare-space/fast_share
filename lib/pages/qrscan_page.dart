c12ad5fb047ab9061d3a63671d4dd6cdd2eb7373
Nightmare-MY <mengyanshou@gmail.com>
2021-06-21 12:50:22 +0800

    add qr scan page

---
+++ lib/pages/qrscan_page.dart
@@ -0,0 +1,79 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:global_repository/global_repository.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class QrScan extends StatefulWidget {
+  const QrScan({Key key}) : super(key: key);
+
+  @override
+  _QrScanState createState() => _QrScanState();
+}
+
+class _QrScanState extends State<QrScan> {
+  Barcode result;
+  QRViewController controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller.pauseCamera();
+    }
+    controller.resumeCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(flex: 4, child: _buildQrView(context)),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildQrView(BuildContext context) {
+    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
+    var scanArea = MediaQuery.of(context).size.width - 100;
+    // To ensure the Scanner view is properly sizes after rotation
+    // we need to listen for Flutter SizeChanged notification and update controller
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: Colors.red,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: scanArea),
+    );
+  }
+
+  bool poped = false;
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) {
+      if (!poped) {
+        Get.back(result: scanData.code);
+        poped = true;
+      }
+      Log.e(scanData.code);
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
