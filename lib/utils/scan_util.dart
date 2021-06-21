3cb84ebfc9e488833797091b07e327be80f0fe2c
Nightmare-MY <mengyanshou@gmail.com>
2021-06-21 12:50:38 +0800

    add qr_code_scanner

--- lib/utils/scan_util.dart
+++ lib/utils/scan_util.dart
@@ -1,12 +1,16 @@
 import 'package:get/get.dart';
 import 'package:global_repository/global_repository.dart';
-import 'package:qrscan/qrscan.dart' as scanner;
 import 'package:speed_share/app/routes/app_pages.dart';
+import 'package:speed_share/pages/qrscan_page.dart';
 
 class ScanUtil {
   static Future<void> parseScan() async {
     await PermissionUtil.requestCamera();
-    final String cameraScanResult = await scanner.scan();
+    final String cameraScanResult = await Get.to(
+      const QrScan(),
+      preventDuplicates: false,
+      fullscreenDialog: true,
+    );
     if (cameraScanResult == null) {
       return;
     }
