46382055b8ae2eedbcd074f98b2048f41c0cf62a
Nightmare <mengyanshou@gmail.com>
2021-08-20 18:47:34 +0800

    upgrade version

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 class Config {
   Config._();
   static int shelfAllPort = 8000;
@@ -7,8 +5,6 @@ class Config {
   static int shelfPort = 8001;
   static int chatPort = 7000;
   static String packageName = 'com.nightmare.speedshare';
-  // static InternetAddress multicastAddress = InternetAddress('224.0.0.1');
   // flutter package名，因为这个会影响assets的路径
   static String flutterPackage = '';
-  static InternetAddress mDnsAddressIPv4 = InternetAddress('224.0.0.251');
 }
