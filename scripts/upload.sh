61a1229c0a2570869c526a1ad42a0be8a995e00a
Nightmare-MY <mengyanshou@gmail.com>
2021-10-23 12:25:07 +0800

    Update upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -4,6 +4,6 @@ if [ -f "速享_macOS.tar" ]; then
     rsync -v "速享_macOS.tar" "$target/速享_$version\_macOS.tar"
 fi
 if [ -f "速享_Windows.zip" ]; then
-    rsync -v "速享_Windows_$version.zip" $target/
+    rsync -v "速享_Windows.zip" "$target/速享_$version\_Windows.zip"
 fi
-rsync -v build/app/outputs/apk/release/app-release.apk "$target/速享_$version\_Android_arm64.apk"
+#rsync -v build/app/outputs/apk/release/app-release.apk "$target/速享_$version\_Android_arm64.apk"
