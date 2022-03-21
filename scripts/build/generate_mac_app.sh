2a5d96cd2e52f6cbd187660b0d8293056ae4da19
mengyanshou <mengyanshou@gmail.com>
2022-03-21 17:50:27 +0800

    chore: commit package script

--- scripts/build/generate_mac_app.sh
+++ scripts/build/generate_mac_app.sh
@@ -7,4 +7,4 @@ rm -rf "$PROJECT_DIR/scripts/dmg/Speed Share.dmg"
 rm -rf "$PROJECT_DIR/scripts/dmg/Speed Share.app"
 cp -rf "$PROJECT_DIR/build/macos/Build/Products/Release/Speed Share.app" $PROJECT_DIR/scripts/dmg/
 $PROJECT_DIR/scripts/dmg/wrapper.sh
-mv "$PROJECT_DIR/scripts/dmg/Speed Share.dmg" $PROJECT_DIR/
+# mv "$PROJECT_DIR/scripts/dmg/Speed Share.dmg" $PROJECT_DIR/
