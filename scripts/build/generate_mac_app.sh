eeb4ff27474efaf1a89778d00262b2b1905f32e8
nightmare <mengyanshou@gmail.com>
2022-11-01 11:20:19 +0800

    chore: update

--- scripts/build/generate_mac_app.sh
+++ scripts/build/generate_mac_app.sh
@@ -1,8 +1,9 @@
-LOCAL_DIR=$(cd `dirname $0`; pwd)
+LOCAL_DIR=$(
+    cd $(dirname $0)
+    pwd
+)
 PROJECT_DIR=$LOCAL_DIR/../..
 source $LOCAL_DIR/../properties.sh
-rm -rf $PROJECT_DIR/build/macos/Build/Products/Release/*
-flutter build macos
 rm -rf "$PROJECT_DIR/scripts/dmg/Speed Share.dmg"
 rm -rf "$PROJECT_DIR/scripts/dmg/Speed Share.app"
 mv -f "$PROJECT_DIR/build/macos/Build/Products/Release/Speed Share.app" $PROJECT_DIR/scripts/dmg/
