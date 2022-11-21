c60d2f31c4dbcbd14d9fd3972e6ce739c338ae55
nightmare <mengyanshou@gmail.com>
2022-11-21 21:14:26 +0800

    Update properties.sh

--- scripts/properties.sh
+++ scripts/properties.sh
@@ -1,5 +1,5 @@
-VERSION='2.1.9'
-VERSION_CODE='56'
+VERSION='2.1.10'
+VERSION_CODE='57'
 TARGET_PATH=root@$server:/home/nightmare/YanTool/resources/SpeedShare
 APP_NAME='SpeedShare'
 APP_NAME_CN='速享'
