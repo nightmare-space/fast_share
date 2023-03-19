daeafb5ef39111f976cf62ef3a512e2566569ab1
nightmare <mengyanshou@gmail.com>
2023-03-19 19:35:55 +0800

    Bump version to 2.2.4

--- scripts/properties.sh
+++ scripts/properties.sh
@@ -1,5 +1,5 @@
-VERSION='2.2.3-beta'
-VERSION_CODE='63'
+VERSION='2.2.4'
+VERSION_CODE='64'
 TARGET_PATH=root@$server:/home/nightmare/YanTool/resources/SpeedShare
 APP_NAME='SpeedShare'
 APP_NAME_CN='速享'
